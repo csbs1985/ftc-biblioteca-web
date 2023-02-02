8e5b11750f5c55cb5238b2b16caa9bbeeb44bc07
Charles Santos <csbs.conta@outlook.com>
2023-02-02 15:11:30 -0300

    [botao.component] criar

--- projects/ftc-biblioteca/angular-lib/src/lib/botao-icone/botao-icone-enum.ts
+++
@@ -1,6 +0,0 @@
-export enum BotaoIconeEnum {
-  RETENCAO = 'retencao',
-  ESCURO = 'escuro',
-  CLARO = 'claro',
-  SAIR = 'sair',
-}
--- projects/ftc-biblioteca/angular-lib/src/lib/botao/botao.component.ts
+++ projects/ftc-biblioteca/angular-lib/src/lib/botao/botao.component.ts
@@ -1,14 +1,19 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'ftc-botao',
   templateUrl: './botao.component.html',
-  styleUrls: ['./botao.component.scss'],
 })
 export class BotaoComponent implements OnInit {
   @Input() texto: string = 'textoBotao';
 
+  @Output() cliqueBotao = new EventEmitter();
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  public clicouBotao() {
+    console.log('clicou');
+  }
 }
