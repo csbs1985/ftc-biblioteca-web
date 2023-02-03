8cb7cf72bc7bc812efdb78441955b1d1d1399595
Charles Santos <csbs.conta@outlook.com>
2023-02-03 13:07:31 -0300

    [multiselect] criar

---
+++ projects/ftc-biblioteca/angular-lib/src/lib/multiselect-inline/multiselect-inline.component.ts
@@ -0,0 +1,26 @@
+import { SelectInterface } from 'projects/ftc-biblioteca/angular-lib/src/interfaces/select.interfaces';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'ftc-multiselect-inline',
+  templateUrl: './multiselect-inline.component.html',
+})
+export class MultiselectInlineComponent implements OnInit {
+  @Input() rotulo: string = '';
+  @Input() conteudo!: SelectInterface[];
+
+  @Output() selectInlineOutput = new EventEmitter<SelectInterface[]>();
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  public selecionarItem(value: SelectInterface): void {
+    this.conteudo.forEach((item) => {
+      if (item.valor === value.valor)
+        item.selecionado = item.selecionado ? false : true;
+    });
+
+    this.selectInlineOutput.emit(this.conteudo);
+  }
+}
